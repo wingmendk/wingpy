7cef7c029c008c4998f9702c2ffdff95b3e43303
Andreas Bækdahl <anb@wingmen.dk>
2026-01-06 09:50:18 +0000

    v1.4.0

--- src/wingpy/cisco/cml.py
+++ src/wingpy/cisco/cml.py
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
-# Copyright (C) 2025 Wingmen Solutions ApS
+# Copyright (C) 2026 Wingmen Solutions ApS
 # This file is part of wingpy, distributed under the terms of the GNU GPLv3.
 # See the LICENSE, NOTICE, and AUTHORS files for more information.
 
